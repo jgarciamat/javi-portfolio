6bbb5c46983a20c6d521f40ab667b5f43e8c1a50
javi <javi@MacBook-Pro.local>
2025-10-13 16:55:47 +0200

    Add Codecov configuration

--- jest.config.cjs
+++ jest.config.cjs
@@ -8,4 +8,18 @@ module.exports = {
   transform: {
     ...tsJestTransformCfg,
   },
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/main.tsx',
+    '!src/vite-env.d.ts',
+  ],
 };
